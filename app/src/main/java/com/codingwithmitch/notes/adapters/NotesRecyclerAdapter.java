98299d725016bb4efba3a851da1d53643687d7f8
Mitch Tabian <mitch@tabian.ca>
2019-01-04 09:24:25 -0800

    recyclerview adapter

---
+++ app/src/main/java/com/codingwithmitch/notes/adapters/NotesRecyclerAdapter.java
@@ -0,0 +1,72 @@
+package com.codingwithmitch.notes.adapters;
+
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.codingwithmitch.notes.R;
+import com.codingwithmitch.notes.models.Note;
+
+
+import java.util.ArrayList;
+
+public class NotesRecyclerAdapter extends RecyclerView.Adapter<NotesRecyclerAdapter.ViewHolder> {
+
+    private ArrayList<Note> mNotes = new ArrayList<>();
+
+    public NotesRecyclerAdapter(ArrayList<Note> mNotes) {
+        this.mNotes = mNotes;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_note_list_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+
+        holder.timestamp.setText(mNotes.get(position).getTimestamp());
+        holder.title.setText(mNotes.get(position).getTitle());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mNotes.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView timestamp, title;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            timestamp = itemView.findViewById(R.id.note_timestamp);
+            title = itemView.findViewById(R.id.note_title);
+
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
