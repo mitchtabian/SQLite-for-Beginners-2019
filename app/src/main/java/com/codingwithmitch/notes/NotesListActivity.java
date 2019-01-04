21b811e48f1f90a81a287aec0cc72ee3c562b614
Mitch Tabian <mitch@tabian.ca>
2019-01-04 09:39:24 -0800

    setting up a recyclerview

--- app/src/main/java/com/codingwithmitch/notes/NotesListActivity.java
+++ app/src/main/java/com/codingwithmitch/notes/NotesListActivity.java
@@ -2,24 +2,71 @@ package com.codingwithmitch.notes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import com.codingwithmitch.notes.adapters.NotesRecyclerAdapter;
 import com.codingwithmitch.notes.models.Note;
+
+import java.util.ArrayList;
 
 public class NotesListActivity extends AppCompatActivity {
 
     private static final String TAG = "NotesListActivity";
 
+    // ui components
+    private RecyclerView mRecyclerView;
+
+    // vars
+    private ArrayList<Note> mNotes = new ArrayList<>();
+    private NotesRecyclerAdapter mNoteRecyclerAdapter;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes_list);
+        mRecyclerView = findViewById(R.id.recyclerView);
 
-        Note note = new Note("some title", "some content for the note", null);
 
-        Log.d(TAG, "onCreate: my first note: " + note.toString());
+        initRecyclerView();
+        insertFakeNotes();
+    }
 
+    private void insertFakeNotes(){
+        for(int i = 0; i < 1000; i++){
+            Note note = new Note();
+            note.setTitle("title #" + i);
+            note.setContent("content #: " + i);
+            note.setTimestamp("Jan 2019");
+            mNotes.add(note);
+        }
+        mNoteRecyclerAdapter.notifyDataSetChanged();
+    }
+
+    private void initRecyclerView(){
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(linearLayoutManager);
+        mNoteRecyclerAdapter = new NotesRecyclerAdapter(mNotes);
+        mRecyclerView.setAdapter(mNoteRecyclerAdapter);
     }
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
