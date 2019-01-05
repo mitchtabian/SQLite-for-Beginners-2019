e56ac590e0f838915af13affd7b34e79a29f9550
Mitch Tabian <mitch@tabian.ca>
2019-01-04 19:44:54 -0800

    drawing parallel lines on an edittext

---
+++ app/src/main/java/com/codingwithmitch/notes/LinedEditText.java
@@ -0,0 +1,54 @@
+package com.codingwithmitch.notes;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class LinedEditText extends android.support.v7.widget.AppCompatEditText {
+
+    private static final String TAG = "LinedEditText";
+
+    private Rect mRect;
+    private Paint mPaint;
+
+
+    // we need this constructor for LayoutInflater
+    public LinedEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        mRect = new Rect();
+        mPaint = new Paint();
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeWidth(2);
+        mPaint.setColor(0xFFFFD966); // Color of the lines on paper
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+
+        // get the height of the view
+        int height = ((View)this.getParent()).getHeight();
+
+        int lineHeight = getLineHeight();
+        int numberOfLines = height / lineHeight;
+
+        Rect r = mRect;
+        Paint paint = mPaint;
+
+        int baseline = getLineBounds(0, r);
+
+        for (int i = 0; i < numberOfLines; i++) {
+
+            canvas.drawLine(r.left, baseline + 1, r.right, baseline + 1, paint);
+
+            baseline += lineHeight;
+        }
+
+        super.onDraw(canvas);
+    }
+
+}
