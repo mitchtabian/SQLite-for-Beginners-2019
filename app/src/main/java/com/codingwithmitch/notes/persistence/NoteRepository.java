034af82d85b3793558fee4a44b3f7c9d55742355
Mitch Tabian <mitch@tabian.ca>
2019-01-07 12:38:17 -0800

    room persistence repository

---
+++ app/src/main/java/com/codingwithmitch/notes/persistence/NoteRepository.java
@@ -0,0 +1,48 @@
+package com.codingwithmitch.notes.persistence;
+
+
+import android.arch.lifecycle.LiveData;
+import android.content.Context;
+
+import com.codingwithmitch.notes.models.Note;
+
+import java.util.List;
+
+public class NoteRepository {
+
+    private NoteDatabase mNoteDatabase;
+
+    public NoteRepository(Context context) {
+        mNoteDatabase = NoteDatabase.getInstance(context);
+    }
+
+    public void insertNoteTask(Note note){
+
+    }
+
+    public void updateNoteTask(Note note){
+
+    }
+
+    public LiveData<List<Note>> retrieveNotesTask() {
+
+        return null;
+    }
+
+    public void deleteNoteTask(Note note){
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
