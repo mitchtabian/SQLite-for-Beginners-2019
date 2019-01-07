f470a9d80be93b5d5710f76c78615cb54a026454
Mitch Tabian <mitch@tabian.ca>
2019-01-07 14:06:00 -0800

    inserting new notes with an asynctask

--- app/src/main/java/com/codingwithmitch/notes/persistence/NoteRepository.java
+++ app/src/main/java/com/codingwithmitch/notes/persistence/NoteRepository.java
@@ -26,8 +26,7 @@ public class NoteRepository {
     }
 
     public LiveData<List<Note>> retrieveNotesTask() {
-
-        return null;
+        return mNoteDatabase.getNoteDao().getNotes();
     }
 
     public void deleteNoteTask(Note note){
